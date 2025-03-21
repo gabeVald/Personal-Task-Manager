559712355be1a479949b9cf55f55c8cf88bfa00c
gmvald <gmvald@uiowa.edu>
2025-03-21 18:01:36 -0500

    initialize personal task manager with Next.js, Tailwind CSS, and essential configurations

---
+++ personal-task-manager/src/pages/index.js
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export default function Home() {
+    return (
+        <div className="grid grid-rows-[10px_1fr_20px] justify-items-center min-h-screen gap-4 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-[5px] row-start-2 items-center sm:items-start">
+                <Separator />
+                <Tabs defaultValue="account" className="w-[]">
+                    <TabsList>
+                        <TabsTrigger value="All">All</TabsTrigger>
+                        <TabsTrigger value="Todays">Todays</TabsTrigger>
+                        <TabsTrigger value="Todos">Todos</TabsTrigger>
+                        <TabsTrigger value="Gottados">Gottados</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="All"></TabsContent>
+                    <TabsContent value="Todays"></TabsContent>
+                    <TabsContent value="Todos"></TabsContent>
+                    <TabsContent value="Gottados"></TabsContent>
+                </Tabs>
+
+                <Separator />
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+                    Learn
+                </a>
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+                    Examples
+                </a>
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+                    Go to nextjs.org →
+                </a>
+            </footer>
+        </div>
+    );
+}
